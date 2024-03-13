eceb4acd3ab9eb9576c73f5b87adbcc2df43e84b
corbanb <corban@x4.studio>
2024-03-13 12:45:49 -0600

    validating commands in cli

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const x = require('x-ray')();
 const fs = require('fs');
-const data = require('./websites/lennysnewsletter-about');
+const path = require('path');
+
+// Read the websites directory
+const websiteFiles = fs.readdirSync(path.join(__dirname, 'websites'));
+
+// Create a map of all the modules in the websites directory
+const modules = {};
+websiteFiles.forEach((file) => {
+  const moduleName = path.basename(file, '.js');
+  modules[moduleName] = require(`./websites/${moduleName}`); // eslint-disable-line
+});
+
+const args = process.argv.slice(2);
+const name = args[0] || 'demo';
+
+// If the module doesn't exist, throw an error
+if (!modules[name]) {
+  throw new Error(`No module found for '${name}'`);
+}
+
+const data = modules[name];
 
 const articles = [];
 const promises = [];
-const { set } = data;
+const { set, filename, urls } = data;
 
-data.urls.forEach((url) => {
-  console.log(url);
+// Define an array to store the names of the missing variables
+const missingVariables = [];
+
+if (!set) {
+  missingVariables.push('set');
+}
+
+if (!filename) {
+  missingVariables.push('filename');
+}
+
+if (!urls) {
+  missingVariables.push('urls');
+}
+
+// If there are missing variables, throw an error with the names of the missing variables
+if (missingVariables.length > 0) {
+  throw new Error(`The following variables are not set: ${missingVariables.join(', ')}`);
+}
+
+urls.forEach((url) => {
   promises.push(
     x(url, set),
   );
@@ -15,17 +54,21 @@ data.urls.forEach((url) => {
 
 Promise.all(promises).then((values) => {
   values.forEach((value) => {
-    console.log(value);
+    // console.log(value);
     articles.push(value);
   });
 
   const json = JSON.stringify({ articles });
 
-  fs.writeFile('articles.json', json, 'utf8', () => {
-    console.log('done');
+  fs.writeFile(filename, json, 'utf8', () => {
+    console.log(
+      '------------------------------------------------------------------------',
+    );
+    console.log(' ');
+    console.log(' ');
+    console.log('🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉 🎉');
+    console.log(' ');
+    console.log('👉 ', filename, 'has been created');
+    console.log(' ');
   });
 });
-
-console.log(
-  '------------------------------------------------------------------------',
-);
